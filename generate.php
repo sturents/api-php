d1f5a0432e5457493909344697f55b62c013ab00
m1ke <git@m1ke.me>
2018-07-27 13:31:29 +0100

    New version of generator

--- generate.php
+++ generate.php
@@ -1,17 +1,27 @@
 #!/usr/bin/env php
 <?php
 use SwaggerGen\GenerateModels;
+use SwaggerGen\GenerateRequests;
 
 require __DIR__.'/vendor/autoload.php';
 
-$root_namespace = 'Models';
-$namespace = "SturentsLib\\Api\\$root_namespace";
-$dir = "src/$root_namespace";
+$namespace = "SturentsLib\\Api";
+$dir = "src/";
+$yaml = __DIR__.'/swagger/api.yml';
+$uri = "https://sturents.com/api";
 
 shell_exec("find ./{$dir} -type f -delete");
 
 $generator = new GenerateModels($namespace);
-$generator->generate(__DIR__.'/swagger/api.yml');
+$generator->generate($yaml);
 $generator->saveClasses($dir);
+$generator->dumpParentClass($dir);
+
+echo "Generated ".count($generator->classes)." files in $dir\n";
+
+$generator = new GenerateRequests($namespace, $uri);
+$generator->generate($yaml);
+$generator->saveClasses($dir);
+$generator->dumpParentClass($dir);
 
 echo "Generated ".count($generator->classes)." files in $dir\n";
